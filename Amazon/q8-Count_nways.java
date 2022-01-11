711db1b6ce7d3efdaff19728a3229362d3d67ef2
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-01-11 09:31:40 +0530

    Create q8-Count_nways.java
    Count n ways to n-th stairs.. Order doesn't matter

---
+++ Amazon/q8-Count_nways.java
@@ -0,0 +1,11 @@
+// https://practice.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter1322/1/#
+class Solution
+{
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    Long countWays(int m)
+    {
+         long x = (long)((m/2)+1);
+         return x;
+    }    
+}
