9a37f71009faa0670b2a87e491dcbfd090bb7694
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-01-23 10:59:39 +0530

    Create q14-Min_steps_destination.java

---
+++ Microsoft/q14-Min_steps_destination.java
@@ -0,0 +1,20 @@
+// https://practice.geeksforgeeks.org/problems/minimum-number-of-steps-to-reach-a-given-number5234/1/#
+class Solution{
+    static int minSteps(int D){
+        int steps=0;
+        int sum=0;
+        
+        while(sum<D){
+            sum+=steps;
+            steps++;
+        }
+        
+        while((sum-D)%2!=0){
+            sum+=steps;
+            steps++;
+        }
+        return steps-1;
+        
+      
+    }
+}
