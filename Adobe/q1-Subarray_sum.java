971839e2176f22538b5a1bf5fabd04a573325bef
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-01-27 23:03:53 +0530

    Update q1-Subarray_sum.java

--- Adobe/q1-Subarray_sum.java
+++ Adobe/q1-Subarray_sum.java
@@ -1,4 +1,4 @@
-// https://practice.geeksforgeeks.org/problems/subarray-with-given-sum-1587115621/1#
+// https://practice.geeksforgeeks.org/problems/subarray-with-given-sum-1587115621/1
 class Solution
 {
     //Function to find a continuous sub-array which adds up to a given number.
